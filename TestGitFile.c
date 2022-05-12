ea5bdc628064219982b808d5b649eebcc7388b8c
AndrewTran03 <andrewtranva@gmail.com>
2022-05-12 17:50:38 -0400

    01 Testing Adding a File from Ubuntu

---
+++ TestGitFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Will include <stdbool.h> and <stdlib.h> later
+
+int main(int argc, char* argv[])
+{
+    printf("Testing Git Pushing --1st Time-- Hopefully it will work!");
+    
+
+    return 0;
+}
