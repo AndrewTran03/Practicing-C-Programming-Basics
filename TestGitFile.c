a9069439fd5c98c3fd5adeeaa535ad51c061165f
AndrewTran03 <andrewtranva@gmail.com>
2022-05-12 18:01:34 -0400

    Edited TestGitFile.c

--- TestGitFile.c
+++ TestGitFile.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-//Will include <stdbool.h> and <stdlib.h> later
+#include <stdbool.h> 
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
     printf("Testing Git Pushing --1st Time-- Hopefully it will work!\n");
     
-    printf("Success on this RLogin side! Testing Git Pushing --2nd Time-- Hopefully it will work!\n");
+    printf("Success on the RLogin side! Testing Git Pushing --2nd Time-- Hopefully it will work!\n");
+
+    printf("Success on the Ubuntu side! Testing Git Pushing --LAST TIME-- Hopefully it will work!\n");
     return 0;
 }
